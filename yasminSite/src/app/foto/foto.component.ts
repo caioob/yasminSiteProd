06568280f25ff373c162f36cd84f82677f2da189
caioob <ritiosbr@protonmail.com>
2020-07-01 18:55:47 -0400

    asa

--- yasminSite/src/app/foto/foto.component.ts
+++ yasminSite/src/app/foto/foto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-foto',
@@ -8,14 +9,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FotoComponent implements OnInit {
 
-  constructor(private router: ActivatedRoute) {
-    tipo: String;
-  }
+  id: Number;
+  foto: Number;
+  fotoAntes: Number;
+  fotoDepois: Number;
+  horizontal: Number;
+
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit(): void {
       this.router.queryParams.subscribe(params => {
-        this.tipo = +params['type'];
+        this.id = +params['id'];
+        this.foto = +params['foto'];
+        this.horizontal = +params['hor'];
+
+        this.fotoAntes = Number(this.foto) - 1 ;
+        this.fotoDepois = Number(this.foto) + 1 ;
+        if(this.fotoAntes == 0) {
+          this.fotoAntes = 1;
+        }
+        else if (this.fotoDepois > 6) {
+          this.fotoDepois = 6;
+        }
       });
+      console.log(this.id);
   }
+
 
 }
